74843af8b4c3d2e7d43071e00a77f160c926dc99
ShevchenkoAlexen <shevchenko.alexen@yandex.ru>
2018-04-10 15:24:04 +0300

    поправил, добавил реактивности

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "./core/auth.service";
-
+import {Subscription} from "rxjs/Subscription";
+import {User} from "./users/user";
+import {UserDataService} from "./users/user-data.service";
 
 
 
@@ -13,15 +15,41 @@ import {AuthService} from "./core/auth.service";
 })
 export class AppComponent {
   title = 'Tra';
-  isAuth: boolean;
-  constructor(private router: Router,   public authService: AuthService ){
+  auth: any;
+  user: User;
+  subscription: Subscription;
+  subscriptionUser: Subscription;
+
+  constructor(private router: Router,   public authService: AuthService, public userService: UserDataService){
+    this.subscription = this.authService.getMessage().subscribe(masseg => {
+      console.log(masseg);
+      this.auth = masseg.auth;
+      if (this.auth){
+        this.login();
+      } else
+      {
+        this.logout()
+      }
+    });
+    this.subscriptionUser = this.userService.getMessage().subscribe( masseg => {
+      console.log('userService.getMessage');
+      console.log(masseg);
+    })
 
   }
   ngOnInit() {
-
   }
+
+  login(){
+    this.userService.getUser();
+  }
+
+  logout(){
+    this.router.navigate(['/']);
+  }
+
+
   onClick(){
-    console.log(this.router);
     this.router.navigate(['/login']);
   }
 
@@ -30,11 +58,16 @@ export class AppComponent {
     this.router.navigate(['/login',isNewUser]);
   }
 
-  isAuthM(){
-    this.isAuth = this.authService.isAuth();
+  isAuth(){
+    if (this.auth) return true;
+    return false;
   }
   onLogOut(){
     this.authService.signOut();
   }
 
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
 }
